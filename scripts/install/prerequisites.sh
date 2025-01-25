65e995cd042cf48db7837769a63a28f252420c1e
DamonT17 <damontregear@gmail.com>
2025-01-25 14:50:56 -0800

    change: formatting of 'echo' statements (pt. 6)

--- scripts/install/prerequisites.sh
+++ scripts/install/prerequisites.sh
@@ -76,7 +76,7 @@ echo -e "This script will install the following core packages:\n"\
 
 # Prompt confirmation before continuing
 if [[ ! $* == *"--force"* ]]; then
-  echo -e "Do you wish to install the prerequisite core packages? (y/N)"
+  echo -en "Do you wish to install the prerequisite core packages? (y/N)"
   read -t 15 -n 1 -r
   echo
   if [[ ! $REPLY =~ ^[Yy]$ ]]; then
